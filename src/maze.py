a4cae9fcb72c4bd66c642ff05652dad837b260c2
can-cali <can.cali@hotmail.com>
2024-08-23 12:30:38 +0300

    added test file for maze

--- src/maze.py
+++ src/maze.py
@@ -19,7 +19,7 @@ class Maze():
         self._cell_size_x = cell_size_x
         self._cell_size_y = cell_size_y
         self._win = win
-        
+
         self._create_cells()
     
     def _create_cells(self):
@@ -30,12 +30,18 @@ class Maze():
                 cell = Cell(self._win)
                 column.append(cell)
             self._cells.append(column)
-        self._draw_cell(self._cell_size_x, self._cell_size_y)
+        for i in range(self._num_cols):
+            for j in range(self._num_rows):
+                self._draw_cell(i, j)
     
     def _draw_cell(self, i, j):
-        for row in range(self._num_rows):
-            for col in range(self._num_cols):
-                self._cells[col][row].draw(i*col, j*row, i*(col+1), j*(row+1))
+        if self._win is None:
+            return
+        x1 = self._x1 + i* self._cell_size_x
+        y1 = self._y1 + j* self._cell_size_y
+        x2 = x1 + self._cell_size_x
+        y2 = y1 + self._cell_size_y
+        self._cells[i][j].draw(x1, y1, x2, y2)
         self._animate()
 
     def _animate(self):
