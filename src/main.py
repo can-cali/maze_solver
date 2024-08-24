9937e9f89234a8babb1f205c6dbdad0f873ce9b5
can-cali <can.cali@hotmail.com>
2024-08-24 17:25:02 +0300

    added breaking walls functionality and tests

--- src/main.py
+++ src/main.py
@@ -6,7 +6,8 @@ from maze import Maze
 
 def main():
     win = Window(800,600)
-    maze = Maze(0, 0, 10, 10, 80, 60, win)
+    maze = Maze(10, 10, 10, 10, 80, 60, win)
+    maze._break_entrance_and_exit()
     win.redraw()
     win.wait_for_close()
 
--- src/maze.py
+++ src/maze.py
@@ -12,13 +12,13 @@ class Maze():
         cell_size_y,
         win,
     ):
-        self._x1 = x1
-        self._y1 = y1
-        self._num_rows = num_rows
-        self._num_cols = num_cols
-        self._cell_size_x = cell_size_x
-        self._cell_size_y = cell_size_y
-        self._win = win
+        self._x1 = x1 # offset from the left side of the window
+        self._y1 = y1 # offset from the top side of the window
+        self._num_rows = num_rows # number of rows
+        self._num_cols = num_cols # number of columns
+        self._cell_size_x = cell_size_x # width of a cell
+        self._cell_size_y = cell_size_y # height of a cell
+        self._win = win # window object to draw the maze
 
         self._create_cells()
     
@@ -37,7 +37,7 @@ class Maze():
     def _draw_cell(self, i, j):
         if self._win is None:
             return
-        x1 = self._x1 + i* self._cell_size_x
+        x1 = self._x1 + i* self._cell_size_x 
         y1 = self._y1 + j* self._cell_size_y
         x2 = x1 + self._cell_size_x
         y2 = y1 + self._cell_size_y
@@ -50,5 +50,14 @@ class Maze():
         self._win.redraw()
         time.sleep(0.05)
         # wait for a short time to see the animation
+    
+    def _break_entrance_and_exit(self):
+        entrance = self._cells[0][0]
+        exit = self._cells[self._num_cols-1][self._num_rows-1]
+        entrance.has_left_wall = False
+        exit.has_right_wall = False
+        self._draw_cell(0, 0)
+        self._draw_cell(self._num_cols-1, self._num_rows-1)
+
 
 
